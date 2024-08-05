1f7f63c000b91ecdb82ac66c9abc6085726f1fbf
souchiyan <souchiyan98@gmail.com>
2024-08-05 10:55:58 +0900

    first commit

---
+++ src/SessionProvider.js
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+import { authRepository } from "./repositories/auth";
+
+const SessionContext = createContext();
+
+const SessionProvider = (props) =>{
+    const [currentUser, setCurrentUser]= useState();
+    const [isLoading, setIsLoading]= useState(true);
+    useEffect(() =>{
+        setSeesion();
+    }, []);
+
+    const setSeesion = async () =>{
+        const currentUser = await authRepository.getCurrentUser();
+        setCurrentUser(currentUser);
+        setIsLoading(false);
+    };
+    if(isLoading) return <div />
+
+    return (
+        <SessionContext.Provider value={{currentUser, setCurrentUser}}>
+            {props.children}
+        </SessionContext.Provider>
+    );
+
+};
+export {SessionContext, SessionProvider}
